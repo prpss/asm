2828a5eb2388956e0d7bc0dea600f86880547f9a
prpss <1ms19is093@gmail.com>
2021-02-18 11:13:06 +0530

    initial commit
    added hello world prog

---
+++ hello_world.s
@@ -0,0 +1,26 @@
+; testing out assembly language
+; nasm -f macho64 hello_world.o hello_world.s && ld -macosx_version_min 10.7.0 -lSystem -o hello_world  hello_world.o && ./hello_world
+
+global start
+
+
+section .text
+
+start:
+    ; start syscall
+    mov     rax, 0x2000004
+    mov     rdi, 1
+    mov     rsi, msg
+    mov     rdx, msg.len
+    syscall
+
+    ; exit syscall
+    mov     rax, 0x2000001
+    mov     rdi, 0
+    syscall
+
+
+section .data
+
+msg:    db      "heya", 10
+.len:   equ     $ - msg
